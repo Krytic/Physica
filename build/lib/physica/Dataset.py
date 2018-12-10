5276635e913ca64e030ea33c851d14c9dd51eb71
Krytic <krytical.programmer@gmail.com>
2018-12-10 20:27:37 +1300

    fix untracked files

--- build/lib/physica/Dataset.py
+++
@@ -1,9 +0,0 @@
-class Dataset:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-
-	def me(self):
-		print(self.x)
-		print(self.y)
-		print(np.arange(3))
--- release.py
+++
@@ -1,9 +0,0 @@
-import os, shutil
-
-for dirname in ['dist', 'build', 'Physica.egg-info']:
-	if os.path.isdir(dirname):
-		shutil.rmtree(dirname)
-
-os.system('python setup.py sdist bdist_wheel')
-
-os.system('python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*')
